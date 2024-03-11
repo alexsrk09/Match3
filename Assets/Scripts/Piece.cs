3e6ccaf087f46454614b08c35ea63add4e1a17f1
mtorresinformatica <89740575+mtorresinformatica@users.noreply.github.com>
2024-03-11 18:58:22 +0100

    Solución Tarea de creación del Match 3 del juego móvil
    Se ha añadido todas las funcionalidades pedidas de la tarea y además algunas animaciones en la eliminación y creación de las piezas.

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -5,10 +5,10 @@ using DG.Tweening;
 
 public class Piece : MonoBehaviour
 {
-
     public int x;
     public int y;
     public Board board;
+
 
     public enum type
     {
@@ -28,27 +28,45 @@ public class Piece : MonoBehaviour
 
     public void Setup(int x_, int y_, Board board_)
     {
-        this.x = x_;
-        this.y = y_;
-        this.board = board_;
+        x = x_;
+        y = y_;
+        board = board_;
+
+        transform.localScale = Vector3.one * 0.35f;
+        transform.DOScale(Vector3.one, 0.35f);
     }
 
-    public void Move(int desx, int desy)
+    public void Move(int desX, int desY)
     {
-        transform.DOMove(new Vector3(desx, desy, -5f), 0.25f).
-            SetEase(Ease.InOutCubic).onComplete = () =>{
-                x = desx;
-                y = desy;
+        transform.DOMove(new Vector3(desX, desY, -5f), 0.25f).SetEase(Ease.InOutCubic).onComplete = () =>
+        {
+            x = desX;
+            y = desY;
         };
+    }
+
+    public void Remove(bool animated)
+    {
+        if (animated)
+        {
+            transform.DORotate(new Vector3(0, 0, -120f), 0.12f);
+            transform.DOScale(Vector3.one * 1.2f, 0.085f).onComplete = () =>
+            {
+                transform.DOScale(Vector3.zero, 0.1f).onComplete = () =>
+                {
+                    Destroy(gameObject);
+                };
+            };
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     [ContextMenu("Test Move")]
     public void MoveTest()
     {
-        Move(0,0);
+        Move(0, 0);
     }
-
-
-
-
 }
